7f22abe209e0e990b23d8e7004c174d2d03ff77d
Jeroyigop <jerie.corales@lorma.edu>
2026-01-26 13:51:33 +0800

    Activity2

---
+++ activity2/ArrayMed.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+public class ArrayMed {
+
+    public static double sumColumn(double[][] m, int columnIndex) {
+        double sum = 0;
+        for (int i = 0; i < m.length; i++) {
+            sum += m[i][columnIndex];
+        }
+        return sum;
+    }
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        double[][] matrix = new double[3][4];
+
+
+        System.out.println("Enter a 3-by-4 matrix row by row:");
+
+
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 4; j++) {
+                matrix[i][j] = input.nextDouble();
+            }
+        }
+
+        for (int col = 0; col < 4; col++) {
+            System.out.println(
+                "Sum of the elements at column " + col + " is " + sumColumn(matrix, col)
+            );
+        }
+
+        input.close();
+    }
+}

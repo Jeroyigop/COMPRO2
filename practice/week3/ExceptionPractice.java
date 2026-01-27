f6049b1777f5dfdbb17930c4409901497dad3f96
Jeroyigop <jerie.corales@lorma.edu>
2026-01-27 13:31:40 +0800

    practice

--- practice/week3/ExceptionPractice.java
+++ practice/week3/ExceptionPractice.java
@@ -1,19 +1,33 @@
 package practice.week3;
 
-import java.util.Scanner
+import java.util.Scanner;
 
 public class ExceptionPractice {
     public static void main(String[] args) {
+        inputNumber();
+    }
 
-        try{ System.out.println("enter a number ");
-        int number = new Scanner(System.in).nextInt();
+
+        public static int inputNumber(){
+            Scanner sc = new Scanner(System.in);
+            int number = 0;
+            boolean value = false;
+
+            try{ System.out.println("enter a number ");
+            number = sc.nextInt();
         System.out.println("you entered: " + number);
         } catch (Exception e){
             System.out.println("Invalid input, please enter an integer.");
         }
+        return number;
+
+        
+        
+        
 
     
     }
     
+
     
 }
